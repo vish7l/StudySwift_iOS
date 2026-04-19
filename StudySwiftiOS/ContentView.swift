8352833ed88cfb1b04ede52fdb3a4870aacd0015
Vishal <vishalsinhadubai@gmail.com>
2026-04-19 06:10:00 -0700

    Initial Commit

---
+++ StudySwiftiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudySwiftiOS
+//
+//  Created by Vishal Sinha on 19/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudySwiftiOS/StudySwiftiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftiOSApp.swift
+//  StudySwiftiOS
+//
+//  Created by Vishal Sinha on 19/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
